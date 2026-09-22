4ce4dea0c51a66de94ac664e2534a34757e161f3
Naveenkumar0005 <naveenbeniwal1088@gmail.com>
2026-09-22 15:43:06 +0930

    Add search and filter restaurant funcationality

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,19 +6,33 @@ import {useState} from "react";
 const Body = () => {
 
 const [restaurantList, setRestaurantList] = useState(restaurants);
+const [searchText, setSearchText] = useState("");
+const [filterRestaurants, setFilterRestaurants] = useState(restaurants);
 
     return (
         <div className="body">
             <div className="filter">
+                <div className="search">
+                    <input type="text" className="search-box" placeholder="search" value={searchText} 
+                    onChange={(e) => { setSearchText(e.target.value);  }}  />
+                    <button className="search-btn" onClick={() => {
+                        // filter logic for search
+                            const filteredRestaurants = restaurantList.filter((restaurant) =>
+                            restaurant.name.toLowerCase().includes(searchText.toLowerCase())
+                        );
+                        setFilterRestaurants(filteredRestaurants);
+                    }}>Search</button>
+                </div>
+
                 <button className="filter-btn" onClick={() => {
                     // filter logic for top rated restaurants
                     const topRatedRestaurants = restaurantList.filter((restaurant) => restaurant.rating >= 4.5);
-                    setRestaurantList(topRatedRestaurants);
+                    setFilterRestaurants(topRatedRestaurants);
                     }}>Top rated Restaurants</button>
             </div>
 
             <div className="restaurant-container">
-                {restaurantList.map((restaurant) => (                 
+                {filterRestaurants.map((restaurant) => (                 
                <RestaurantCard key={restaurant.id} restobj={restaurant} />
                 ))}
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import { foodLogo } from "../utils/constants";
+import {useState} from "react";
 
 const Header = () => {
+    const [btnName, setBtnName] = useState("Login");
     return (
         <div className="header">          
         <div className="logo-container">
@@ -12,6 +14,10 @@ const Header = () => {
                 <li>About Us</li>
                 <li>Contact Us</li>
                 <li>Cart</li>
+                <button className="login-btn" onClick={() =>{
+                    btnName === "Login" ? setBtnName("LogOut") : setBtnName("Login");             
+                   
+                }}>{btnName}</button>
                 
             </ul>
         </div>
